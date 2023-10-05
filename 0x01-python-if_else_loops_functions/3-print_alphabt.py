93680cb062310e03c3a7af7386cd1fd486a0dc33
Onwuamaegbu Ndubuisi <onwuamaegbund99@gmail.com>
2023-10-04 18:58:11 -0700

    Updated

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for char in range(ord('a'), ord('z') + 1):
-    if ord('q') is not char and ord('e') is not i:
+    if ord('q') is not char and ord('e') is not char:
         print("{}".format(chr(char)), end='')
