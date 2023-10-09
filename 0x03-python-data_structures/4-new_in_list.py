5985aa55a9e355beffb81909bf9a141b655a1d83
Onwuamaegbu Ndubuisi <onwuamaegbund99@gmail.com>
2023-10-09 09:16:53 -0700

    Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -7,7 +7,7 @@ def new_in_list(my_list, idx, element):
     new_list = my_list
     if idx < 0:
         return my_list
-    elif idx > list_length:
+    elif idx >= list_length:
         return my_list
     else:
         new_list = my_list.copy()
