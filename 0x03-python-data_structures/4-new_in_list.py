60dd720fc04eba4c17975ed3c1319cc67a910e1a
Onwuamaegbu Ndubuisi <onwuamaegbund99@gmail.com>
2023-10-09 09:10:52 -0700

    Replace in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    """Replaces an element in a list at a specific position without modifying the original list"""
+
+    list_length = len(my_list)
+    new_list = my_list
+    if idx < 0:
+        return my_list
+    elif idx > list_length:
+        return my_list
+    else:
+        new_list = my_list.copy()
+        new_list[idx] = element
+        return new_list
+
