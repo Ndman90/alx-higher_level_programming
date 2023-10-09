f301bf1d15456e5e35379c9f61aaba4b9c67e468
Onwuamaegbu Ndubuisi <onwuamaegbund99@gmail.com>
2023-10-09 08:16:40 -0700

    function that prints all integers of a list, in reverse order

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    """function that prints all integers of a list, in reverse order"""
+    """function that prints all integers of a list,
+    in reverse order"""
     list_length = len(my_list)
     index = list_length - 1
 
