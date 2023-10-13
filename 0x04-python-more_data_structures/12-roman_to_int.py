f19efcaf6e6a554634787f0cfb263043b40e7be2
Onwuamaegbu Ndubuisi <onwuamaegbund99@gmail.com>
2023-10-12 18:37:54 -0700

    Function that converts a Roman numeral to an integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,26 +4,26 @@ def roman_to_int(roman_string):
     """Converts a Roman numeral to an integer"""
     if roman_string is None or type(roman_string) is not str:
         return 0
-    total = 0  """ To store the total integer value"""
-    prev_value = 1000  """ To handle subtractive notation"""
 
-    """ Dictionary to map Roman numerals to integers"""
     roman_dict = {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
     }
+    num = 0
 
-    """ Convert Roman numerals to integers and process them"""
-    for numeral in roman_string:
-        current_value = roman_dict.get(numeral, 0)
-        if current_value > prev_value:
-            total -= prev_value * 2
-        prev_value = current_value
-        total += current_value
+    for i in range(len(roman_string)):
+        if roman_dict.get(roman_string[i], 0) == 0:
+            return (0)
 
-    return (total)
+        if (i != (len(roman_string) - 1) and
+                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
+            num += roman_dict[roman_string[i]] * -1
+
+        else:
+            num += roman_dict[roman_string[i]]
+    return (num)
