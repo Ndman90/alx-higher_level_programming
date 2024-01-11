c60cb565d4cba60d405dc6838ccc7ef58004fb11
Ndman90 <onwuamaegbund99@gmail.com>
2024-01-10 20:52:51 -0800

    Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,18 +2,11 @@
 /**
  * Check the parameters provided
  */
-
 class Rectangle {
   constructor (w, h) {
-    // Check if w and h are positive integers
-    if (w > 0 && Number.isInteger(w) && h > 0 && Number.isInteger(h)) {
-      // Initialize instance attributes
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
       this.width = w;
       this.height = h;
-    } else {
-      // If w or h is equal to 0 or not a positive integer, create an empty object
-      this.width = 0;
-      this.height = 0;
     }
   }
 }
